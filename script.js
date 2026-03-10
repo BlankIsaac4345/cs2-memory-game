6570ebd9b4b8024b36e179d260afbd6be344591d
BlankIsaac4345 <isaacml4345@gmail.com>
2026-03-10 18:33:34 +0000

    update

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log("Script started")
+
+function flipcard(cardId){
+    console.log("click");
+
+    let card = document.getElementById(cardId);
+    card.style.color = "black";
+    card.style.backgroundColor = "white";
+}
